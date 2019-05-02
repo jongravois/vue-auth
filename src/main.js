9e7afade45fc9ee71fb3f05d08a9fe68ddd8dadc
Jonathan Gravois <jongravois@gmail.com>
2019-05-02 11:09:33 -0500

    clean up to add service

--- src/main.js
+++ src/main.js
@@ -1,12 +1,28 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import firebase from 'firebase';
+import App from './App.vue';
+import router from './router';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+let app = '';
+const config = {
+  apiKey: "AIzaSyAezqwnL-Zjf_zKtCD9RL14ChrwfA0oxOI",
+  authDomain: "vue-firebase-d087f.firebaseapp.com",
+  databaseURL: "https://vue-firebase-d087f.firebaseio.com",
+  projectId: "vue-firebase-d087f",
+  storageBucket: "vue-firebase-d087f.appspot.com",
+  messagingSenderId: "387428730329"
+};
+
+firebase.initializeApp(config);
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    /* eslint-disable no-new */
+    app = new Vue({
+      router,
+      render: h => h(App)
+    }).$mount('#app');
+  }
+});
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="logout">Logout</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  // @ is an alias to /src
+  import firebase from 'firebase';
+  import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'home',
+    components: {
+      HelloWorld
+    },
+    methods: {
+      logout: function() {
+        //alert(process.env.VUE_APP_SPACES);
+        firebase.auth().signOut().then(() => {
+          this.$router.replace('login')
+        })
+      }
+    }
   }
-}
 </script>
